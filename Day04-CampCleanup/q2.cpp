995d1c825b3eb94a63b0ec0b7f9cb66e22f3773b
OmarAlzoubi <omaralzoubi2045@gmail.com>
2022-12-05 02:38:47 +0300

    Day04 Part01 && Part02

---
+++ Day04-CampCleanup/q2.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+
+
+
+int main()
+{
+
+    std::ifstream myFile;
+    myFile.open("input.txt");
+    std::string line;
+
+    int result = 0;
+
+    while(getline(myFile, line))
+    {
+        std::string p1 = line.substr(0, line.find(','));
+        std::string p2 = line.substr(line.find(',') + 1);
+
+        int p1Start = stoi(p1.substr(0, '-'));
+        int p1End = stoi(p1.substr(p1.find('-') + 1));
+
+        int p2Start = stoi(p2.substr(0, '-'));
+        int p2End = stoi(p2.substr(p2.find('-') + 1));
+
+        if (((p1Start <= p2End) && (p1End >= p2Start)))
+        {
+            result++;
+        }
+    }
+
+    std::cout << result;
+
+
+}
